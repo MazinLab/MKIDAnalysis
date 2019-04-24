b514ef77416d1833c5d806769c4a6cc9ff764210
Isabel Lipartito <ilipartito@umail.ucsb.edu>
2019-04-23 17:36:56 -0700

    Added clipped_zoom fxn from Rupert

--- analysis_utils.py
+++ analysis_utils.py
@@ -75,6 +75,51 @@ def rotate_shift_image(image, degree, xshift, yshift):
     rotated_shifted = np.roll(xshifted_image, yshift, axis=0)
     return rotated_shifted
 
+def clipped_zoom(img, zoom_factor, **kwargs):
+    """ Courtesy of
+    https://stackoverflow.com/questions/37119071/scipy-rotate-and-zoom-an-image-without-changing-its-dimensions """
+
+    h, w = img.shape[:2]
+    # For multichannel images we don't want to apply the zoom factor to the RGB
+    # dimension, so instead we create a tuple of zoom factors, one per array
+    # dimension, with 1's for any trailing dimensions after the width and height.
+    zoom_tuple = (zoom_factor,) * 2 + (1,) * (img.ndim - 2)
+
+    # Zooming out
+    if zoom_factor < 1:
+        # Bounding box of the zoomed-out image within the output array
+        zh = int(np.round(h * zoom_factor))
+        zw = int(np.round(w * zoom_factor))
+        top = (h - zh) // 2
+        left = (w - zw) // 2
+        # Zero-padding
+        out = np.zeros_like(img)
+        out[top:top + zh, left:left + zw] = zoom(img, zoom_tuple, **kwargs)
+
+    # Zooming in
+    elif zoom_factor > 1:
+        # Bounding box of the zoomed-in region within the input array
+        zh = int(np.round(h / zoom_factor))
+        zw = int(np.round(w / zoom_factor))
+        top = (h - zh) // 2
+        left = (w - zw) // 2
+        out = zoom(img[top:top + zh, left:left + zw], zoom_tuple, **kwargs)
+        # `out` might still be slightly larger than `img` due to rounding, so
+        # trim off any extra pixels at the edges
+        trim_top = ((out.shape[0] - h) // 2)
+        trim_left = ((out.shape[1] - w) // 2)
+        if trim_top < 0 or trim_left < 0:
+            temp = np.zeros_like(img)
+            temp[:out.shape[0], :out.shape[1]] = out
+            out = temp
+        else:
+            out = out[trim_top:trim_top + h, trim_left:trim_left + w]
+
+    # If zoom_factor == 1, just return the input array
+    else:
+        out = img
+    return out
+
 
 def interpolate_image(image, method='linear'):
     '''
